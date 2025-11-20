b6a350c5c3ceed8e00a8637efaecf180ada31d7a
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-20 09:12:35 +0200

    set turnstile in form

--- src/state/cart.js
+++ src/state/cart.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 
 export const useCart = create((set) => ({
 
-    cart: {1:{quantity:100}},
+    cart: {},
     success: { state: null, item: {} },
 
     add: (payload) => set((state) => {
