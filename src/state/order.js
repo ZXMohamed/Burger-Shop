8ffa4610fee72229f59fb9f3ea19fae663e335ba
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-11-25 02:20:14 +0200

    currency change

--- src/state/order.js
+++ src/state/order.js
@@ -1,22 +1,7 @@
 import { create } from "zustand";
 
 export const useOrder = create((set) => ({
-    order: {
-        k45: {
-            order: { 1: { quantity: 2 } },
-            orderCode: "k45",
-            orderInfo: {
-                HNo: "value.homeNumber",
-                city: "value.city",
-                country: "value.country",
-                state: "value.state",
-                pinCode: "value.pinCode",
-                phoneNo: "value.phoneNumber",
-                status: "processing",
-                paymentMethod: "COD"
-            }
-        }
-},
+    order: {},
     add: (payload) => set((state) => ({
         ...state,
         order: {
