77b0324480f3a7322480ce03b0ff7ce660ea78b3
ZXMoahmed <mhmdalshwamy@outlook.com>
2025-12-18 21:34:43 +0200

    cart.js state edit

--- src/state/cart.js
+++ src/state/cart.js
@@ -4,19 +4,11 @@ import { create } from "zustand";
 export const useCart = create((set) => ({
 
     cart: {},
-    success: { state: null, item: {} },
 
     add: (payload) => set((state) => {
 
-        if (state.cart[payload.id]) {
-        
-            return ({
-                ...state,
-                success: { state: false, item: payload }
-            })
-            
-        } else {
-            
+        if (!state.cart[payload.id]) {
+
             return ({
                 ...state,
                 success: { state: true, item: payload },
